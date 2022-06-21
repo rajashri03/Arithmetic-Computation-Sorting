b750323302b8ed780c105ea1a2b18fbea511eb94
rajashri15 <rajashristelvelar315@gmail.com>
2022-06-21 12:08:24 +0530

    UC1-Take 3 inputs from user

---
+++ artihmatic.sh
@@ -0,0 +1,10 @@
+#UC1-Write a program to take 3 inputes -a ,b &c
+echo "Sorting Arithmatic Computation Problems"
+read -p "Enter value of a:" a
+read -p "Enter value of b:" b
+read -p "Enter value of c:" c
+echo "a=$a b=$b c=$c"
+
+#In Uc 1 we have to take 3 inputes only and just we have to print 
+#values for a b and c 
+#read -p example, this means you want to execute read using the p option, which stands for prompt.

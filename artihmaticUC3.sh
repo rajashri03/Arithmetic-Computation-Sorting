d9f3ca2349aff1e31cd6e6e56d78d85dcf1a0e99
rajashri15 <rajashristelvelar315@gmail.com>
2022-06-21 12:30:40 +0530

    UC3--a*b*c

---
+++ artihmaticUC3.sh
@@ -0,0 +1,11 @@
+#UC2-Compute a + b * c
+echo "Sorting Arithmatic Computation Problems"
+read -p "Enter value of a:" a
+read -p "Enter value of b:" b
+read -p "Enter value of c:" c
+echo "a=$a b=$b c=$c"
+result=$(($a+$b*c))
+echo "a+b*c=" $result
+result2=$(($a*$b*$c))
+echo "a*b*c=" $result2
+
